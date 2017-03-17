499ed1717f74ccf0dcf6191aa72d629558fb3066
Primoz Z <pzagor2@gmail.com>
2017-03-17 18:36:49 +0100

    Added simple options menu for managing what to filter

--- myscript.js
+++ myscript.js
@@ -1,35 +1,49 @@
 console.log('Start Youtube Filter EXT');
 //Helper functions
 function getDataFromGridItem(item) {
-	const user = item.querySelector('a.g-hovercard').innerText;
-	const title = item.querySelector('.yt-lockup-title').querySelector('a').innerText;
+  const user = item.querySelector('a.g-hovercard').innerText;
+  const title = item.querySelector('.yt-lockup-title').querySelector('a').innerText;
 
-	const data = {
-		user: user,
-		title: title,
-		dom: item
-	};
-	return data;
+  const data = {
+    user: user,
+    title: title,
+    dom: item
+  };
+  return data;
 }
 
-//Get all item into data array
-const DOMItems = document.querySelectorAll('.yt-shelf-grid-item');
-const data = [...DOMItems].map(item => {
-	return getDataFromGridItem(item);
-});
-//Filter out NBA items
-const onlyNba = data.filter(item => {
-	if (item.user === 'NBA') {
-		return true;
-	}
-	return false;
-});
-//Keep only title with TOP
-const notTOP = onlyNba.filter(item => {
-	var upperTitle = item.title.toUpperCase();
-	return !upperTitle.includes('TOP');
+chrome.storage.sync.get({
+  keep: []
+}, storedData => {
+
+  //Get all item into data array
+  const DOMItems = document.querySelectorAll('.yt-shelf-grid-item');
+  const data = [...DOMItems].map(item => {
+    return getDataFromGridItem(item);
+  });
+  storedData.keep.forEach(sotedItem => {
+
+    //Filter out NBA items
+    const onlyTitle = data.filter(item => {
+      if (item.user === sotedItem.channel) {
+        return true;
+      }
+      return false;
+    });
+
+    //Keep only title with TOP
+    const notTOP = onlyTitle.filter(item => {
+      const upperTitle = item.title.toUpperCase();
+      return !upperTitle.includes(sotedItem.tag.toUpperCase());
+    });
+
+    for(item of notTOP) {
+      item.dom.style.display = 'none';
+    }
+
+  });
 });
 
-for(item of notTOP) {
-	item.dom.style.display = 'none';
-}
+
+
+
---
+++ options.js
@@ -0,0 +1,40 @@
+//debug URL: chrome-extension://hnajbpkopgmkoaebdhallmmkdmanbilo/options.html
+let data;
+// Saves options to chrome.storage.sync.
+function save_options() {
+  const channelName = document.getElementById('channel').value;
+  const tag = document.getElementById('tags').value;
+
+
+  data.keep.push({
+    channel: channelName,
+    tag: tag
+  });
+
+
+  chrome.storage.sync.set(data, () => {
+    // Update status to let user know options were saved.
+    const status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+  restore_options();
+}
+
+
+function restore_options() {
+  chrome.storage.sync.get({
+    keep: []
+  }, items => {
+    data = items;
+    document.getElementById('keepTags').innerHTML = JSON.stringify(data.keep);
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('add').addEventListener('click', save_options);
+document.getElementById('clear').addEventListener('click', () => {
+  chrome.storage.sync.clear();
+  restore_options();
+});
